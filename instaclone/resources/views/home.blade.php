4571139dfced4cdc9b36ee7421cb628fdb1c9a4e
Haunty <haunty@noreply.github.com>
2020-11-24 23:15:36 +0100

    instaclone design parts

---
+++ instaclone/resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+        <img src="https://scontent-amt2-1.cdninstagram.com/v/t51.2885-19/s150x150/20838890_520279488310819_7712602893756399616_a.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_ohc=6FwzL3fqA94AX8D8EHT&tp=1&oh=9f6a9b35035ee12a80b713b66c534707&oe=5FE5E80A" class="rounded-circle">
+        </div>
+
+        <div class="col-9 pt-5">
+            <div><h1>AccountName</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>143</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">something.org</div>
+            <div>teksaidjfgouieaoijsdoijaoisjdoisajoidsjaiodjoiajosid</div>
+            <div><a href="#">www.test.tests</a></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-4">
+            <img class="w-100 pt-5" src="https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/fr/e15/s1080x1080/126324359_3466729130113067_6980135892308876116_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=1&_nc_ohc=bBmdFqKkiskAX9C_kw6&tp=1&oh=81658aa589170076cf26a4455d39d934&oe=5FE5ECE6" alt="image">
+        </div>
+        <div class="col-4">
+            <img class="w-100 pt-5" src="https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/fr/e15/s1080x1080/126324359_3466729130113067_6980135892308876116_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=1&_nc_ohc=bBmdFqKkiskAX9C_kw6&tp=1&oh=81658aa589170076cf26a4455d39d934&oe=5FE5ECE6" alt="image">
+        </div>
+        <div class="col-4">
+            <img class="w-100 pt-5" src="https://scontent-amt2-1.cdninstagram.com/v/t51.2885-15/fr/e15/s1080x1080/126324359_3466729130113067_6980135892308876116_n.jpg?_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_cat=1&_nc_ohc=bBmdFqKkiskAX9C_kw6&tp=1&oh=81658aa589170076cf26a4455d39d934&oe=5FE5ECE6" alt="image">
+        </div>
+    </div>
+</div>
+@endsection
